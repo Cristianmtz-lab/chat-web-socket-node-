32f3117398452cd738500af23c78069172a9e80c
blackcode2 <crija19@gmail.com>
2025-02-11 20:44:26 -0500

    server socket.on inserta messages en turso, io.emit add junto al msg, el lastinsertrowid = serverOffser. se valida socket recovered para recuperar los msg anteriores y enviar a client y render.

--- server/index.js
+++ server/index.js
@@ -1,8 +1,12 @@
 import express from 'express'
 import logger from 'morgan'
+import dotenv from 'dotenv'
+import { createClient } from '@libsql/client'
 
 import { Server } from 'socket.io'
 import { createServer } from 'node:http'
+
+dotenv.config()
 
 const port = process.env.PORT ?? 3000
 
@@ -10,16 +14,55 @@ const app = express()
 const server = createServer(app)
 const io = new Server(server)
 
-io.on('connection', (socket) => {
+const turso = createClient({
+  url: "libsql://chat-socket-cristianmtz-lab.turso.io",
+  authToken: process.env.DB_TOKEN,
+})
+
+await turso.execute(`
+    CREATE TABLE IF NOT EXISTS messages (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      content TEXT
+    )
+  `)
+
+io.on('connection', async (socket) => {
   console.log('a user a connected!')
 
   socket.on('disconnect', () => {
     console.log('an user has disconnected')
   })
 
-  socket.on('chat message', (msg) => {
-    io.emit('chat message', msg)
+  socket.on('chat message', async (msg) => {
+    let result
+    try {
+      result = await turso.execute({
+        sql: 'INSERT INTO messages (content) VALUES (:msg)',
+        args: { msg }
+      })
+    } catch (error) {
+      console.error(error)
+      return
+    }
+    io.emit('chat message', msg, result.lastInsertRowid.toString())
   })
+
+  if (!socket.recovered) {
+    try {
+      const results = await turso.execute({
+        sql: 'SELECT id, content FROM messages WHERE id > ?',
+        args: [socket.handshake.auth.serverOffset ?? 0]
+      })
+
+      results.rows.forEach(row => {
+        socket.emit('chat message', row.content, row.id.toString())
+      })
+      console.log(results)
+    } catch (error) {
+      console.error(error)
+    }
+
+  }
 
 })
 
